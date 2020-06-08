ade9b779af254e432cc5da52cd3fd5f2fd20bf0f
jaydeep <jaydeeppatidar87@gmail.com>
2020-06-08 20:57:39 +0530

    update length file

--- lengthofstring.py
+++ lengthofstring.py
@@ -1,4 +1,7 @@
 print('program to print length of given string \n Enter a string')
-s=eval(input())
-print('length of string is',len(s))
+s=input()
+l=0
+for x in s:
+    l+=1
+print('length of string is',l)
 input()
